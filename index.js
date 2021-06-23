65879016c4f7244a919952391156e0565392ccfa
Pedro Correa <pedro8correa@gmail.com>
2021-06-22 23:10:40 -0300

    fix: fixing build rust project and added js fibonacci example

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+const { promisify } = require('util');
 const addon = require('./index.node');
+
+const fibonacci = async value => {
+    let f0 = 0;
+    let f1 = 1;
+    for (let x = 1; x < value; x++) {
+        let f2 = f0 + f1;
+        f0 = f1;
+        f1 = f2;
+    }
+    return f0;
+}
 
 function helloWorldExample() {
     const { hello } = addon;
@@ -13,17 +25,30 @@ function functionExample() {
 
 function promiseExample() {
     const { myAsyncFunc } = addon;
-    myAsyncFunc(10)
-        .then(result => {
-            console.log('[PromiseExample] myAsyncFunc(10) ->', result);
-        })
+
+    const fibAsync = promisify(myAsyncFunc)
+
+    console.time('fibonacci')
+    fibAsync(1000)
+        .then((result) => console.log('[PromiseExample] myAsyncFunc(1000)', result))
+        .catch((error) => console.error('error', error))
+        .finally(() => console.timeEnd('fibonacci'))
 }
+
+function pureJs() {
+    console.time('fibonacci')
+    fibonacci(1000)
+        .then((result) => console.log('[PromiseExample] myAsyncFunc(1000)', result))
+        .catch((error) => console.error('error', error))
+        .finally(() => console.timeEnd('fibonacci'))
+}
+
 
 const map = {
     hello: helloWorldExample,
     func: functionExample,
-    async: promiseExample
+    async: promiseExample,
+    js: pureJs,
 };
-
 
 map[process.argv[2]]()
--- src/promise.rs
+++ src/promise.rs
@@ -6,7 +6,7 @@ use std::mem::replace;
 fn compute(n: usize) -> BigUint {
     let mut f0: BigUint = Zero::zero();
     let mut f1: BigUint = One::one();
-    for _ in 0..n {
+    for _ in 1..n {
         let f2 = f0 + &f1;
         // This is a low cost way of swapping f0 with f1 and f1 with f2.
         f0 = replace(&mut f1, f2);
@@ -14,30 +14,24 @@ fn compute(n: usize) -> BigUint {
     f0
 }
 
-struct FibonacciTask {
-    argument: usize,
-}
-
-impl Task for FibonacciTask {
-    type Output = BigUint;
-    type Error = ();
-    type JsEvent = JsString;
-
-    fn perform(&self) -> Result<BigUint, ()> {
-        Ok(compute(self.argument))
-    }
-
-    fn complete(self, mut cx: TaskContext, result: Result<BigUint, ()>) -> JsResult<JsString> {
-        Ok(cx.string(result.unwrap().to_str_radix(10)))
-    }
-}
-
 pub fn fibonacci_async(mut cx: FunctionContext) -> JsResult<JsUndefined> {
-    let n = cx.argument::<JsNumber>(0)?.value() as usize;
-    let cb = cx.argument::<JsFunction>(1)?;
+    let n = cx.argument::<JsNumber>(0)?.value(&mut cx) as usize;
+    let cb = cx.argument::<JsFunction>(1)?.root(&mut cx);
+    let queue = cx.queue();
 
-    let task = FibonacciTask { argument: n };
-    task.schedule(cb);
+    std::thread::spawn(move || {
+        let result = compute(n);
+        queue.send(move |mut cx| {
+            let callback = cb.into_inner(&mut cx);
+            let this = cx.undefined();
+            let args = vec![
+                cx.null().upcast::<JsValue>(),
+                cx.string(result.to_str_radix(10)).upcast()
+            ];
+            callback.call(&mut cx, this, args)?;
+            Ok(())
+        });
+    });
 
     Ok(cx.undefined())
 }
